e61a2fa26335ee7cb063de92d888bfcbe5992d0e
Temitope Bankole <bankson03@gmail.com>
2023-05-01 18:59:34 +0100

    pagination added

--- src/components/Repository.vue
+++ src/components/Repository.vue
@@ -20,7 +20,7 @@
           </tr>
         </thead>
         <tbody class="tab-body">
-          <tr v-for="(repo, i) in repos" :key="repo.id">
+          <tr v-for="(repo, i) in displayedRepos" :key="repo.id">
             <td>{{ i + 1 }}</td>
             <td>{{ repo.name }}</td>
             <td><a :href="repo.html_url" target="_blank">{{ repo.url }}</a></td>
@@ -29,6 +29,11 @@
         </tbody>
       </table>
       <p v-if="isLoading">Loading...</p>
+    </div>
+    <div>
+       <button @click="prevPage" :disabled="currentPage === 1">Previous</button>
+      <button v-for="page in pages" :key="page" @click="goToPage(page)" :class="{ active: page === currentPage }">{{ page }}</button>
+      <button @click="nextPage" :disabled="endIndex >= repos.length">Next</button>
     </div>
   </div>
 </template>
@@ -41,6 +46,9 @@ export default {
     return {
       repos: [],
       isLoading: false,
+      currentPage: 1,
+      displayedRepos: [],
+      itemsPerPage: 5,
     };
   },
   created() {
@@ -51,8 +59,50 @@ export default {
       this.repos = JSON.parse(reposData);
     }
     this.isLoading = false;
+    this.updateDisplayRepos(); //call method to update displayed repos
+  },
+
+  computed: {
+    startIndex() {
+      return (this.currentPage - 1) * this.itemsPerPage;
+    },
+    endIndex() {
+      return this.startIndex + this.itemsPerPage;
+    },
+    totalPages() {
+      return Math.ceil(this.repos.length / this.itemsPerPage);
+    },
+     pages() {
+   const pagesArray = [];
+      for (let i = 1; i <= this.totalPages; i++) {
+        pagesArray.push(i);
+      }
+      return pagesArray;
+    },
+  },
+  methods: {
+    nextPage() {
+      this.currentPage++;
+      this.updateDisplayRepos(); // call method to update displayed repos
+    },
+    prevPage() {
+      this.currentPage--;
+      this.updateDisplayRepos();  // call method to update displayed repos
+    },
+    goToPage(page) {
+      this.currentPage = page;
+      this.updateDisplayRepos();
+    },
+    updateDisplayRepos() {
+       // update the displayed repos
+      // slice the repos array based on the current page and update the displayed repos
+      this.displayedRepos = this.repos.slice(this.startIndex, this.endIndex);
+    },
   },
 };
+
+
+
 </script>
 
 <style>
